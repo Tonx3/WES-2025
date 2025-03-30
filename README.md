7229d98fce728f5765621d0eaeb060c40e4699cf
Marko <msrpak001@gmail.com>
2025-03-30 19:48:29 +0200

    Add basics

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # WES-2025
 Workshop on Embedded Systems - Hackaton
+
+## How to start
+
+1. Download with 
+```
+git clone https://github.com/Tonx3/WES-2025.git
+```
+or with 
+```
+git clone git@github.com:Tonx3/WES-2025.git
+```
+if you are contributor.
+
+2. **IMPORTANT** never push on main branch, immediately do
+```
+git checkout develop
+```
+
+3. Use
+```
+git submodule update --init --recursive
+```
+(**NOTE**: DON'T USE EDUROAM)
+
+4. Position to components/lvgl_esp32_drivers using 
+```
+cd components/lvgl_esp32_drivers
+```
+
+5. Apply a patch using
+```
+git apply ../lvgl_esp32_drivers_8-3.patch
+```
+
+6. Go back to main folder
+```
+cd ../../
+```
+
+7. You can now build and flash the device.
---
+++ components/gui/app.c
@@ -0,0 +1,44 @@
+/**
+ *
+ * ░█──░█ ░█▀▀▀ ░█▀▀▀█ ── █▀█ █▀▀█ █▀█ █▀▀
+ * ░█░█░█ ░█▀▀▀ ─▀▀▀▄▄ ▀▀ ─▄▀ █▄▀█ ─▄▀ ▀▀▄
+ * ░█▄▀▄█ ░█▄▄▄ ░█▄▄▄█ ── █▄▄ █▄▄█ █▄▄ ▄▄▀
+ *
+ */
+
+/*--------------------------- INCLUDES ---------------------------------------*/
+#include "app.h"
+#include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+
+// lvgl
+#include "lvgl.h"
+#include "lvgl_helpers.h"
+
+// squareline
+#include "ui.h"
+/*--------------------------- MACROS AND DEFINES -----------------------------*/
+/*--------------------------- TYPEDEFS AND STRUCTS ---------------------------*/
+/*--------------------------- STATIC FUNCTION PROTOTYPES ---------------------*/
+static void _app_task(void *p_parameter);
+/*--------------------------- VARIABLES --------------------------------------*/
+/*--------------------------- STATIC FUNCTIONS -------------------------------*/
+
+static void _app_task(void *p_parameter)
+{
+    (void)p_parameter;
+    for (;;) {
+        printf("Hello world\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+/*--------------------------- GLOBAL FUNCTIONS -------------------------------*/
+
+void app_init(void)
+{
+    ui_init();
+    xTaskCreatePinnedToCore(_app_task, "app", 4096 * 2, NULL, 0, NULL, 0);
+}
